2f08b8df656d6a65945400d9b64c9c5e82221863
abreham1222 <abreham1222@gmail.com>
2022-09-20 22:25:29 +0300

    Add book title method

--- main.js
+++ main.js
@@ -43,16 +43,4 @@ document.forms[0].onsubmit = (event) => {
   const title = thisForm[0].value;
   const author = thisForm[1].value;
 
-  if (title === '' || author === '') {
-    const section = document.getElementById('form-section');
-    const message = document.createElement('p');
-    message.innerHTML = 'Please put something into the fields';
-    section.insertAdjacentElement('afterend', message);
-    setTimeout(() => { message.remove(); }, 3000);
-  } else {
-    books.addBook(title, author);
-    thisForm[0].value = '';
-    thisForm[1].value = '';
-  }
-  window.localStorage.setItem('books', JSON.stringify(books));
-};
+  
