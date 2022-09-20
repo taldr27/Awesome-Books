801416f8922c61505078a42ee20b3d9251472cdb
taldr27 <diegogarciaq98@gmail.com>
2022-09-20 10:16:31 -0500

    Add classes to the main.js file

--- main.js
+++ main.js
@@ -1,51 +1,49 @@
-let books = [];
-
 const booksContainer = document.getElementById('books-cont');
-function removeBook(index) {
-  books.splice(index, 1);
-  window.localStorage.setItem('books', JSON.stringify(books));
-}
 
-function displayBooks() {
-  booksContainer.innerHTML = '';
-  for (let i = 0; i < books.length; i += 1) {
-    const book = document.createElement('li');
-    book.innerHTML = `<span class="title">'${books[i].title}' by ${books[i].author}</span>  `;
-    const btn = document.createElement('button');
-    btn.className = 'list-btn';
-    const hrLine = document.createElement('hr');
-    btn.textContent = 'Remove';
-    book.append(btn);
-    booksContainer.append(hrLine);
-    btn.onclick = () => {
-      removeBook(i);
-      displayBooks();
-    };
-    booksContainer.append(book);
+class Collect {
+  constructor() {
+    this.books = [];
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    window.localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  displayBooks() {
+    booksContainer.innerHTML = '';
+    for (let i = 0; i < this.books.length; i += 1) {
+      const book = document.createElement('li');
+      book.innerHTML = `<span class="title">'${this.books[i].title}' by ${this.books[i].author}</span>`;
+      const btn = document.createElement('button');
+      btn.className = 'list-btn';
+      const hrLine = document.createElement('hr');
+      btn.textContent = 'Remove';
+      book.append(btn);
+      booksContainer.append(hrLine);
+      btn.onclick = () => {
+        this.removeBook(i);
+        this.displayBooks();
+      };
+      booksContainer.append(book);
+    }
+  }
+
+  addBook(title, author) {
+    this.books.push({ title, author });
+    this.displayBooks();
   }
 }
 
-function addBook(title, author) {
-  books.push({ title, author });
-  displayBooks();
-}
+const books = new Collect();
 
 document.forms[0].onsubmit = (event) => {
   event.preventDefault();
-
   const thisForm = event.target;
   const title = thisForm[0].value;
   const author = thisForm[1].value;
-
-  addBook(title, author);
+  books.addBook(title, author);
   thisForm[0].value = '';
   thisForm[1].value = '';
   window.localStorage.setItem('books', JSON.stringify(books));
-};
-
-window.onload = () => {
-  if (localStorage.getItem('books')) {
-    books = JSON.parse(localStorage.getItem('books'));
-  }
-  displayBooks();
 };
